adfb961f78976e620a82e23d5b3965728b8590c4
HOANG PHI HUNG <hung.t174800@vanlanguni.vn>
2019-05-18 17:17:00 +0700

    update 3001

--- Laptopshop/Laptopshop/Controllers/HomeController.cs
+++ Laptopshop/Laptopshop/Controllers/HomeController.cs
@@ -86,75 +86,7 @@ namespace Laptopshop.Controllers
             return Redirect(loginUrl.AbsoluteUri);
 
         }
-        public ActionResult FacebookCallback(string code)
-
-        {
-            var fb = new FacebookClient();
-
-            dynamic result = fb.Post("oauth/access_token", new
-
-            {
-
-                client_id = "919134721597050",
-
-                client_secret = "48322d66a0188fb4de97b85a4d0be9fc",
-
-                redirect_uri = RediredtUri.AbsoluteUri,
-
-                code = code
-            });
-
-            var accessToken = result.access_token;
-
-            Session["AccessToken"] = accessToken;
-
-            fb.AccessToken = accessToken;
-
-            dynamic me = fb.Get("me?fields=link,first_name,currency,last_name,email,gender,locale,timezone,verified,picture,age_range");
-
-            string email = me.email;
-            string userName = me.email;
-            string firstname = me.first_name;
-            string middlename = me.middle_name;
-            string lastname = me.last_name;
-            Session["email"] = me.email;
-           
-            var user = new Customer();
-            user.Email = email;
-            user.Id = firstname;
-            user.Activated = true;
-            user.Fullname = firstname + " " + lastname;
-            user.Password = "tainganh";
-            user.Photo = "User.png";
-            var model = db.Customers.SingleOrDefault(x => x.Id == user.Id);
-            if (model == null)
-            {
-                db.Customers.Add(user);
-                Session["facebook"] = user;
-                Session["first_name"] = me.first_name;
-
-                Session["lastname"] = me.last_name;
-
-                Session["picture"] = me.picture.data.url;
-                db.SaveChanges();
-            }
-
-            Session["facebook"] = user;
-            Session["first_name"] = me.first_name;
-
-            Session["lastname"] = me.last_name;
-
-            Session["picture"] = me.picture.data.url;
-            FormsAuthentication.SetAuthCookie(email, false);
-            var RequestUrl = Session["RequestUrl"] as String;
-            if (RequestUrl != null)
-            {
-                return Redirect(RequestUrl);
-            }
-            
-            return RedirectToAction("Index", "Home");
-
-        }
+     
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
